26ec681da9e1fe41f606f49324be9d69a396d62c
aylingurel1 <aylngurel@gmail.com>
2024-10-05 13:53:27 +0300

    Proje dosyası ekle.

---
+++ FinalProjesi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProjesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
